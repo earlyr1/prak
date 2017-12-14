25acf4b4bfc67bcd40f28dbdf33c4bb41276b660
earlyr1 <s02140286@stud.cs.msu.ru>
2017-12-14 19:55:33 +0300

    add derivatives

--- hea/heat.cpp
+++ hea/heat.cpp
@@ -39,13 +39,13 @@ double a2(double t) {
 
 
 int main(int argc, char ** argv) {
-	ofstream ofs("data.txt");
-	int mode = 2;
+	int mode = atoi(argv[5]);
 	M = atoi(argv[1]); // N per x 
 	P = atoi(argv[2]); // N per time	
 	T = atof(argv[3]); // complete time
 	char * edge1 = argv[4]; //0 if values; 1 if derivative
 	edge = argv[4][0] == 'd'? 1:0;
+	ofstream ofs("data" + to_string(mode) + to_string(edge) + ".txt");
 	dx = 1.0 / M;
 	dt = (T + 0.0) / P;
 	prevLayer = new double[M + 1](); 
@@ -57,20 +57,37 @@ int main(int argc, char ** argv) {
 	if (mode == 1) {
 		
 		double * tmp;
-		for(int i = 0; i <= M; i++) prevLayer[i] = phi(dx * i);		
-		for(int t = 1; t <= P; t++) {
-			for(int x = 0; x <= M; x++) {
-				if (x == 0) currLayer[x] = a1(t * dt);
-				else if (x == M) currLayer[x] = a2(t * dt);
-				else currLayer[x] = (f(dx * x, dt * (t - 1)) + a * a * ((prevLayer[x - 1] - 2 * prevLayer[ x] + prevLayer[ x + 1]) / (dx * dx))) * dt + prevLayer[x];
+		for(int i = 0; i <= M; i++) prevLayer[i] = phi(dx * i);	
+		if (!edge) 	{
+			for(int t = 1; t <= P; t++) {
+				for(int x = 0; x <= M; x++) {
+					if (x == 0) currLayer[x] = a1(t * dt);
+					else if (x == M) currLayer[x] = a2(t * dt);
+					else currLayer[x] = (f(dx * x, dt * (t - 1)) + a * a * ((prevLayer[x - 1] - 2 * prevLayer[ x] + prevLayer[ x + 1]) / (dx * dx))) * dt + prevLayer[x];
+				}
+				tmp = prevLayer;
+				prevLayer = currLayer;
+				currLayer = tmp;
 			}
-			tmp = prevLayer;
-			prevLayer = currLayer;
-			currLayer = tmp;
+		} else {
+			for(int t = 1; t <= P; t++) {
+				for(int x = 1; x < M; x++) {
+					currLayer[x] = (f(dx * x, dt * (t - 1)) + a * a * ((prevLayer[x - 1] - 2 * prevLayer[ x] + prevLayer[ x + 1]) / (dx * dx))) * dt + prevLayer[x];
+				}
+				currLayer[0] = (4 * currLayer[1] - currLayer[2]) / 3;
+				currLayer[M] = (4 * currLayer[M - 1] - currLayer[M - 2]) / 3;
+
+				tmp = prevLayer;
+				prevLayer = currLayer;
+				currLayer = tmp;
+			}
 		}
+		double error = 0.;
 		for(int i = 0; i < M + 1; i++) {
 			ofs << (i + 0.) / M << " " << prevLayer[i] << " " <<  ans[i] << endl;
+			error += (ans[i] - prevLayer[i]) * (ans[i] - prevLayer[i]);
 		}
+		cout << mode << " " << edge << " " << error << endl;
 	} else {
 		for(int i = 0; i < M + 1; i++) {
 			prevLayer[i] = currLayer[i];
@@ -79,31 +96,71 @@ int main(int argc, char ** argv) {
 		alpha = new double[M + 1]();
 		beta = new double[M + 1]();
 		for(int i = 0; i <= M; i++) currLayer[i] = phi(dx * i);
-		for ( int t = 0; t < P; t++ ){
-			tmp = prevLayer;
-			prevLayer = currLayer;
-			currLayer = tmp;
-			int x;
-			alpha[0] = 0;
-			beta[0] = 0;
-			int xi1 = a1(t * dt) / prevLayer[1];
-			int mu1 = a1(t * dt) - prevLayer[1]*xi1;
-			alpha[1] = xi1;
-			beta[1] = mu1;
-			for ( x = 1; x < M; x++ ){
-				alpha[x+1] = dt*a*a/(dx*dx) / ( 1 + 2*dt*a*a/(dx*dx) - alpha[x]*dt*a*a/(dx*dx) );
-				beta[x+1] = ( dt*f(x*dx,t*dt) + prevLayer[x] + beta[x]*(dt*a*a)/(dx*dx) ) / ( 1 + 2*dt*a*a/(dx*dx) - alpha[x]*dt*a*a/(dx*dx) );
+		if (!edge) {
+			for ( int t = 0; t < P; t++ ){
+				tmp = prevLayer;
+				prevLayer = currLayer;
+				currLayer = tmp;
+				int x;
+				alpha[0] = 0;
+				beta[0] = 0;
+				int X1 = a1(t * dt) / prevLayer[1];
+				int mew1 = a1(t * dt) - prevLayer[1]*X1;
+				alpha[1] = X1;
+				beta[1] = mew1;
+				for ( x = 1; x < M; x++ ){
+					alpha[x+1] = dt*a*a/(dx*dx) / ( 1 + 2*dt*a*a/(dx*dx) - alpha[x]*dt*a*a/(dx*dx) );
+					beta[x+1] = ( dt*f(x*dx,t*dt) + prevLayer[x] + beta[x]*(dt*a*a)/(dx*dx) ) / ( 1 + 2*dt*a*a/(dx*dx) - alpha[x]*dt*a*a/(dx*dx) );
+				}
+				int X2 = a2(t * dt) / prevLayer[x-1];
+				int mew2 = a2(t * dt) - prevLayer[x-1]*X2;
+				currLayer[M] = (X2*beta[x]+mew2)/(1-X2*alpha[x]);
+				for ( int x = M; x > 0/*1*/; x-- ){
+					currLayer[x-1] = alpha[x]*currLayer[x]+beta[x];
+				}		
 			}
-			int xi2 = a2(t * dt) / prevLayer[x-1];
-			int mu2 = a2(t * dt) - prevLayer[x-1]*xi2;
-			currLayer[M] = (xi2*beta[x]+mu2)/(1-xi2*alpha[x]);
-			for ( int x = M; x > 0/*1*/; x-- ){
-				currLayer[x-1] = alpha[x]*currLayer[x]+beta[x];
+		} else {
+			for ( int t = 0; t < P; t++ ){
+				int X1, mew1, X2, mew2;
+				tmp = prevLayer;
+				prevLayer = currLayer;
+				currLayer = tmp;
+				int x;
+				alpha[0] = 0;
+				beta[0] = 0;
+				if (!edge) {
+					X1 = 0;
+					mew1 = a1(t * dt);
+				} else {
+					X1 = 1;
+					mew1 = 0;
+				}
+				alpha[1] = X1;
+				beta[1] = mew1;
+				for ( x = 1; x < M; x++ ){
+					alpha[x+1] = dt*a*a/(dx*dx) / ( 1 + 2*dt*a*a/(dx*dx) - alpha[x]*dt*a*a/(dx*dx) );
+					beta[x+1] = ( dt*f(x*dx,t*dt) + prevLayer[x] + beta[x]*(dt*a*a)/(dx*dx) ) / ( 1 + 2*dt*a*a/(dx*dx) - alpha[x]*dt*a*a/(dx*dx) );
+				}
+				if (!edge) {
+					X2 = 0;
+					mew2 = a2(t * dt);
+				} else {
+					X2 = 1;
+					mew2 = 0;
+				}
+				currLayer[M] = (X2*beta[x]+mew2)/(1-X2*alpha[x]);
+				for ( int x = M; x > 1/*1*/; x-- ){
+					currLayer[x-1] = alpha[x]*currLayer[x]+beta[x];
+				}
+				currLayer[0] = (4 * currLayer[1] - currLayer[2]) / 3;
+				currLayer[M] = (4 * currLayer[M - 1] - currLayer[M - 2]) / 3;
 			}
-			
 		}
+		double error = 0.;
 		for(int i = 0; i < M + 1; i++) {
 			ofs << (i + 0.) / M << " " << currLayer[i] << " " <<  ans[i] << endl;
+			error += (ans[i] - currLayer[i]) * (ans[i] - currLayer[i]);
 		}
+		cout << mode << " " << edge << " " << error << endl;
 	}
 }
